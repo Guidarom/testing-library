3b6c64fdc38bc722f6c4b551e9fa2dc54c1e2026
Guidarom <guidodaniel.davila@gmail.com>
2023-09-14 14:04:15 +0200

    coverage added

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   detectOpenHandles: true,
-  verbose: true
+  verbose: true,
+  collectCoverage:true,
+  coverageDirectory:"./coverage"
 };
